5c16ef34247ec793a28f43d76b6b86c0531937f4
yangqianjuan <1072483500@qq.com>
2023-09-19 10:09:19 +0800

    add demo

--- packages/hooks/src/useBoolean/demo/demo1.tsx
+++ packages/hooks/src/useBoolean/demo/demo1.tsx
@@ -1,4 +1,21 @@
 import React from 'react';
 import { useBoolean } from 'reactHooks';
 
-export default function () {}
+export default function () {
+  const [state, { setTrue, setFalse, toggle }] = useBoolean(true);
+
+  return (
+    <div>
+      <p>state:{`${state}`}</p>
+      <div>
+        <button onClick={setTrue}>setTrue</button>
+      </div>
+      <div>
+        <button onClick={setFalse}>setFalse</button>
+      </div>
+      <div>
+        <button onClick={toggle}>toggle</button>
+      </div>
+    </div>
+  );
+}
